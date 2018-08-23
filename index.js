bacd7aa2439819d2670a26a572a243e52c28b2b3
daniellamarr <danielchidiebele@gmail.com>
2018-08-23 07:08:39 +0100

    Integrate travisci

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import Joi from 'joi';
 import express from 'express';
-import Route from './mvc/routes/route';
 import bodyParser from 'body-parser';
+import Route from './mvc/routes/route';
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,6 +12,6 @@ Route(app);
 // PORT
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+const server = app.listen(port, () => port);
 
 export default server;
