9b46bdbd1909b5bfed0c73c2f97cd5ae28c0869f
daniellamarr <danielchidiebele@gmail.com>
2018-08-23 05:21:19 +0100

    Setup the linting library

--- mvc/controllers/controller.js
+++ mvc/controllers/controller.js
@@ -3,6 +3,31 @@ import answers from '../structures/answerStructure';
 
 class Controller {
 
+    getAllQuestions = (req, res) => {
+        res.send(questions);
+    }
+
+    askQuestion = (req, res) => {
+    
+        if (!req.body.title || req.body.title.length < 3) {
+            res.status(400).send('Please add the title for your question');
+            return;
+        }
+    
+        const question = {
+            id: questions.length + 1,
+            title: req.body.title
+        };
+        questions.push(question);
+        res.send(question);
+    }
+    
+    getOneQuestion = (req, res) => {
+        const question = questions.find(c => c.id === parseInt(req.params.id));
+        if (!question) res.status(404).send('This question may have been deleted');
+        res.send(question);
+    }
+    
     postAnswer = (req, res) => {
     
         if (!req.body.reply || req.body.reply.length < 3) {
--- mvc/routes/route.js
+++ mvc/routes/route.js
@@ -2,6 +2,12 @@ import Controller from "../controllers/controller";
 
 const Route = (app) => {
 
+    app.get('/api/v1/questions', Controller.getAllQuestions);
+
+    app.get('/api/v1/questions/:id', Controller.getOneQuestion)
+
+    app.post('/api/v1/questions', Controller.askQuestion)
+
     app.post('/api/v1/questions/:id/answers', Controller.postAnswer)
 
 }
